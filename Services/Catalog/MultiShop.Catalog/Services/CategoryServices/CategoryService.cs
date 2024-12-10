99d0132779758b6c78a4265907e612c277a1a0aa
hfuatkarahan <hfkarahan@aof.anadolu.edu.tr>
2024-12-10 19:26:23 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categories;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);   //Bağlantı client nesnesine atandı
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);  // client sayesinde veritabanına gittik.
+            _categories = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);   //Burda da tabloya ulaştık
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+            await _categories.InsertOneAsync(value);
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            await _categories.DeleteOneAsync(x=>x.CategoryId == id);
+        }
+
+        public async Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            var values = await _categories.Find<Category>(x=>x.CategoryId==id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdCategoryDto>(values);
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            var values = await _categories.Find(x=> true).ToListAsync();
+            return _mapper.Map<List<ResultCategoryDto>>(values);
+        }
+
+        public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var values = _mapper.Map<Category>(updateCategoryDto);
+            await _categories.FindOneAndReplaceAsync(x=>x.CategoryId == updateCategoryDto.CategoryId, values);
+        }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly DapperContext _dapperContext;
+
+        public DiscountService(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public async Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto)
+        {
+            string query = "insert into Coupons (Code,Rate,IsActive,ValidDate) values (@code,@rate,@isActive,@validDate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", createCouponDto.Code);
+            parameters.Add("@rate", createCouponDto.Rate);
+            parameters.Add("@isActive", createCouponDto.IsActive);
+            parameters.Add("@validDate", createCouponDto.ValidDate);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public Task DeleteDiscountCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
